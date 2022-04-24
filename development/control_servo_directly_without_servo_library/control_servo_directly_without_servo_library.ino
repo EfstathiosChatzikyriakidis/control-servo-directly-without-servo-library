f8cdc4a97d85147ba7e4fdbc61edabe8220c1501
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:28:43 +0300

    Change.

--- development/control_servo_directly_without_servo_library/control_servo_directly_without_servo_library.ino
+++ development/control_servo_directly_without_servo_library/control_servo_directly_without_servo_library.ino
@@ -1,7 +1,7 @@
 /*
  *  Control Servo Directly (Without Servo Library).
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
